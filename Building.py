2d79b41b400e45909dc581fa2a7fc6c9719d939b
Dianna <dhummel.1210@gmail.com>
2017-07-18 20:41:02 -0400

    adding building class file, failed to load with previous commit

---
+++ Building.py
@@ -0,0 +1,11 @@
+# Building class
+
+class Building:
+
+	def __init__(self, real_estate_id, total_units, recycle_units, apt_sc_sqft,
+				heated_area):
+		self.__real_estate_id = real_estate_id
+		self.__total_units = total_units
+		self.__recycle_units = recycle_units
+		self.__apt_sc_sqft = apt_sc_sqft
+		self.__heated_area = heated_area
